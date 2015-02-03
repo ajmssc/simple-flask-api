dd7e5d43b35638bed70afab9d9880b99122aca13
Jean-Marc Soumet <jeanmarc.soumet@relateiq.com>
2015-02-03 10:26:13 -0800

    edit python and html

--- config.py
+++ config.py
@@ -4,12 +4,43 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MYSQL_DATABASE_HOST = 'localhost'
-    MYSQL_DATABASE_PORT = 3306
-    MYSQL_DATABASE_USER = None
-    MYSQL_DATABASE_PASSWORD = None
-    MYSQL_DATABASE_DB = None
-    MYSQL_DATABASE_CHARSET = 'utf8'
+    TITLE = 'Bitcoin Inspector'
+    YOUTUBE_URL = 'https://www.youtube.com/watch?v=sE2rKoDDk3Q'
+    EMAIL_ADDRESS = 'jeanmarc.soumet@gmail.com'
+    GITHUB_URL = 'http://github.com/ajmssc/'
+    LINKEDIN_URL = 'https://www.linkedin.com/in/jeanmarcsoumet'
+    PAGES = [
+        {
+            'name' : 'Home',
+            'link' : '/index',
+            'icon' : 'fa-home'
+        },
+        {
+            'name' : 'Presentation Slides',
+            'link' : '/presentation',
+            'icon' : 'fa-slideshare'
+        },
+        {
+            'name' : 'Historical Data',
+            'link' : '/history_blocks',
+            'icon' : 'fa-line-chart'
+        },
+        {
+            'name' : 'Live Data',
+            'link' : '/live',
+            'icon' : 'fa-bolt'
+        },
+        {
+            'name' : 'Wallets',
+            'link' : '/wallets',
+            'icon' : 'fa-money'
+        },
+        {
+            'name' : 'Range query',
+            'link' : '/wallets_timed',
+            'icon' : 'fa-clock-o'
+        }
+    ]
 
     @staticmethod
     def init_app(app):
